8a6553324d15163fac556f74aa3a4b6f84f4f79f
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-14 18:28:05 -0800

    Minification; meta type

--- blog.sh
+++ blog.sh
@@ -2,8 +2,9 @@
 
 MID=${1%.md}.htm
 HTML=${1%.md}.html
+TITLE=`head -n 1 $1`
 
 pandoc -f markdown $1 > $MID
-cat template.html | sed s/**CONTENT**/cat $MID/e > $HTML
+cat template.html | sed -e "s/CONTENT/cat $MID/e" -e "s/HEADER/$TITLE/" > $HTML
 
-#../publish.sh $HTML
+./publish.sh $HTML
--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-html-minifier $1 -o $1.min
+html-minifier --collapse-boolean-attributes --colapse-inline-tag-whitespace --collapse-whitespace --html5 --minify-css 1 --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-empty-elements --remove-optional-elements --remove-redundant-attributes --remove-tag-whitespace $1 -o $1.min
 scp $1.min alyssa@rosenzweig.io:/var/www/html/$1
