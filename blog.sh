d8e8136b7df09b44eb761557f95a7474eb6708fd
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-01-25 09:50:09 -0800

    Mustache-based blogging

--- blog.sh
+++ blog.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-MID=${1%.md}.htm
 HTML=${1%.md}.html
 TITLE=`head -n 1 $1`
 
-pandoc -f markdown $1 > $MID
-cat template.html | sed -e "s/CONTENT/cat $MID/e" -e "s/HEADER/$TITLE/" > $HTML
+MID=$(pandoc -f markdown $1 | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))')
+VIEW="{\"header\": \"$TITLE\", \"content\": $MID}"
 
-./publish.sh $HTML
+echo $VIEW | mustache - template.html -o $HTML
