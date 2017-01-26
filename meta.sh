f0fda2200786f7bb6a0b602ac5aa9d1928e82067
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-01-26 08:59:51 -0800

    Index with dates

--- meta.sh
+++ meta.sh
@@ -2,6 +2,7 @@
 
 HTML=${1%.md}.html
 TITLE=$(head -n 1 $1 | python -c 'import json,sys; print(json.dumps(sys.stdin.read().strip()))')
+DATE=$(head -n 3 $1 | tail -n 1 | stripmd)
 LEADER=$(head -n 5 $1 | tail -n 1 | stripmd | sed -e 's/[\.\(\!\?].*/.../' | tr '\n' ' ' | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))')
 
-echo '{"title": '$TITLE', "href": "/'$HTML'", "leader": '$LEADER'},'
+echo '{"title": '$TITLE', "href": "/'$HTML'", "leader": '$LEADER', "date": "'$DATE'"},'
