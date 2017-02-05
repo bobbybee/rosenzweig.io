4eb25ee52ee5d13eace29b7ec457224c6e926359
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-04 21:41:52 -0800

    Fix pandoc weirdness

--- markindex.sh
+++ markindex.sh
@@ -1,1 +1,2 @@
-pandoc -f markdown -t html index.md -s -c global.css > index.html
+echo '<style>' > /tmp/open && echo '</style>'> /tmp/close
+pandoc -f markdown -t html index.md --standalone --self-contained -H /tmp/open -H global.css -H /tmp/close> index.html
