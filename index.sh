86ca080d6fd16e8557a59d1038f393582fe508aa
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-05 20:08:34 -0800

    typofix

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm blog/index.md
-find blog/*.md -exec ./sredate.sh {} \;
+find blog/*.md -exec ./redate.sh {} \;
 
 cat blog-list.md > blog/index.md
 ls -1 --sort=time blog/*.md | sed -e s-blog/index.md-- | xargs -n 1 ./meta.sh >> blog/index.md
