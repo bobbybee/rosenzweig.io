5067a6203fc09c85575bc3af0c0193c4c150d373
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-10 19:48:14 -0800

    Emsmarten

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 FILE=${1%.md}
 echo "<style>" > /tmp/open && echo "</style>" > /tmp/close
-pandoc -f markdown -t html $FILE.md --standalone --self-contained -H /tmp/open -H global.css $([[ $FILE == blog* ]] && echo "-H blog.css") -H /tmp/close > $FILE.html
+pandoc -f markdown -t html $FILE.md --smart --standalone --self-contained -H /tmp/open -H global.css $([[ $FILE == blog* ]] && echo "-H blog.css") -H /tmp/close > $FILE.html
 html-minifier --conservative-collapse --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --decode-entities --html5 --minify-css 1 --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-empty-elements --remove-optional-tags --remove-redundant-attributes --remove-tag-whitespace --use-short-doctype $FILE.html > $FILE.min
 ./upload.sh $FILE.min $FILE.html
 
