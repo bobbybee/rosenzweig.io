ec86872ff7ac912b33a62a162cd967d2ad70554a
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-11 12:18:51 -0800

    Streamline presentation system

--- publish.sh
+++ publish.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 FILE=${1%.md}
+
 echo "<style>" > /tmp/open && echo "</style>" > /tmp/close
-pandoc -f markdown -t html $FILE.md --smart --standalone --self-contained -H /tmp/open -H global.css $([[ $FILE == blog* ]] && echo "-H blog.css") -H /tmp/close > $FILE.html
+
+if [[ $FILE == *presentation ]]
+then
+    STYLE="-H present.css --section-divs"
+elif [[ $FILE == blog/* ]]
+then
+    STYLE="-H global.css -H blog.css"
+else
+    STYLE="-H global.css"
+fi
+
+pandoc -f markdown -t html $FILE.md --smart --standalone --self-contained -H /tmp/open $STYLE -H /tmp/close > $FILE.html
+
 html-minifier --conservative-collapse --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --decode-entities --html5 --minify-css 1 --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-empty-elements --remove-optional-tags --remove-redundant-attributes --remove-tag-whitespace --use-short-doctype $FILE.html > $FILE.min
 ./upload.sh $FILE.min $FILE.html
 
