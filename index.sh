7ad5be80b6389f1b290daa9b3c1047a2c2be1250
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-13 09:13:20 -0800

    Filter out presentations in the index script

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find blog/*.md -exec ./redate.sh {} \;
+find blog/*.md -not -name "*presentation.md" -exec ./redate.sh {} \;
 
 cat blog-list.md > blog/index.md
-ls -1 --sort=time blog/*.md | sed -e s-blog/index.md-- | xargs -n 1 ./meta.sh >> blog/index.md
+ls -1 --sort=time blog/*.md | sed -e s-blog/index.md-- | sed -e s-.*presentation.md-- | xargs -n 1 ./meta.sh >> blog/index.md
