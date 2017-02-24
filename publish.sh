a323c4c9ff2cedd8763349b7394ac9cba427ff07
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-24 06:51:42 -0800

    Migrate to pandoc 2

--- publish.sh
+++ publish.sh
@@ -2,19 +2,17 @@
 
 FILE=${1%.md}
 
-echo "<style>" > /tmp/open && echo "</style>" > /tmp/close
-
 if [[ $FILE == *presentation ]]
 then
-    STYLE="-H present.css --section-divs"
+    STYLE="--css present.css --section-divs"
 elif [[ $FILE == blog/* ]]
 then
-    STYLE="-H global.css -H blog.css"
+    STYLE="--css global.css --css blog.css"
 else
-    STYLE="-H global.css"
+    STYLE="--css global.css"
 fi
 
-pandoc -f markdown -t html $FILE.md --smart --standalone --self-contained -H /tmp/open $STYLE -H /tmp/close > $FILE.html
+pandoc -f markdown+smart -t html $FILE.md --standalone --self-contained $STYLE > $FILE.html
 
 html-minifier --conservative-collapse --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --decode-entities --html5 --minify-css 1 --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-empty-elements --remove-optional-tags --remove-redundant-attributes --remove-tag-whitespace --use-short-doctype $FILE.html > $FILE.min
 ./upload.sh $FILE.min $FILE.html
